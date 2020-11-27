7e2e1a6db2950fa59f39020404175eac3ad14e5f
= <=>
2020-11-27 10:37:13 +0100

    Print a BST

--- bst.cpp
+++ bst.cpp
@@ -95,7 +95,6 @@ public:
         return height;
     }
     void prettyPrint();
-
 private:
     BSTNode *head = nullptr;
     int height;
@@ -103,6 +102,7 @@ private:
     int totalNodes;
     std::vector<BSTNode *> allnodes;
     void printLine(int width);
+    std::vector<int> sortedVec;
     void printLevel(int level);
     std::vector<BSTNode *> getVectorFromLevel(int level);
     void setTotalNodes(int num)
@@ -113,6 +113,7 @@ private:
     {
         return totalNodes;
     }
+    void calcInOrder(BSTNode* node);
 };
 
 BST::BST()
@@ -136,6 +137,7 @@ void BST::deleteTree(BSTNode *node)
     deleteTree(node->getRight());
     delete node;
 }
+
 void BST::insertKey(int newKey)
 {
     int nodelevel = 0;
@@ -237,37 +239,18 @@ bool BST::hasKey(int searchKey)
 }
 std::vector<int> BST::inOrder()
 {
-    BSTNode *curr = nullptr;
-    std::vector<int> sortedVec;
-    std::stack<BSTNode *> sortStack;
-    bool done = false;
-    curr = head;
-    if (this->getHeight() == 0)
-    {
-        sortedVec.resize(0);
-        return sortedVec;
+    calcInOrder(head);
+    std::vector<int> temp = sortedVec;
+    sortedVec.clear();
+    return temp;
+}
+void BST::calcInOrder(BSTNode* node){
+    if(node == nullptr){
+        return;
     }
-    while (!done)
-    {
-        while (curr != nullptr)
-        {
-            sortStack.push(curr);
-            curr = curr->getLeft();
-        }
-
-        if (curr == nullptr && !sortStack.empty())
-        {
-            BSTNode *temp = sortStack.top();
-            sortStack.pop();
-            sortedVec.push_back(temp->getVal());
-            curr = temp->getRight();
-        }
-        if (curr == nullptr && sortStack.empty())
-        {
-            done = true;
-        }
-    }
-    return sortedVec;
+    calcInOrder(node->getLeft());
+    sortedVec.push_back(node->getVal());
+    calcInOrder(node->getRight());
 }
 void BST::printLine(int width)
 {
@@ -288,6 +271,7 @@ void BST::printLevel(int level)
         {
             if (i == valsFromLevel.at(j)->getCol())
             {
+  
                 if (i == getTotalNodes()) //last element
                 {
                     if (valsFromLevel.at(j)->getVal() < 0 || valsFromLevel.at(j)->getVal() >= 10)
@@ -319,105 +303,104 @@ void BST::printLevel(int level)
                 }
             }
         }
-            if (!isInLevel)
-            {
-                if (i == getTotalNodes())
-                {
-                    std::cout << "|    |";
-                }
-                else
-                {
-                    std::cout << "|    ";
-                }
-            }
-            isInLevel = false;
-        }
-        std::cout << std::endl;
-    }
-    std::vector<BSTNode *> BST::getVectorFromLevel(int level)
-    {
-
-        std::vector<int> sortedNodes = inOrder();
-        std::vector<BSTNode *> vecFromLevel;
-
-        for (int i = 0; i < sortedNodes.size(); i++)
+        if (!isInLevel)
         {
-            for (int j = 0; j < allnodes.size(); j++)
+            if (i == getTotalNodes())
             {
-                if (sortedNodes.at(i) == allnodes.at(j)->getVal())
+                std::cout << "|    |";
+            }
+            else
+            {
+                std::cout << "|    ";
+            }
+        }
+        isInLevel = false;
+    }
+    std::cout << std::endl;
+}
+std::vector<BSTNode *> BST::getVectorFromLevel(int level)
+{
+    std::vector<int> sortedNodes = inOrder();
+    std::vector<BSTNode *> vecFromLevel;
+
+    for (int i = 0; i < sortedNodes.size(); i++)
+    {
+        for (int j = 0; j < allnodes.size(); j++)
+        {
+            if (sortedNodes.at(i) == allnodes.at(j)->getVal())
+            {
+                if (allnodes.at(j)->getLevel() == level)
                 {
-                    if (allnodes.at(j)->getLevel() == level)
-                    {
-                        vecFromLevel.push_back(allnodes.at(j));
-                    }
+                    vecFromLevel.push_back(allnodes.at(j));
                 }
             }
         }
-
-        return vecFromLevel;
     }
-    void BST::prettyPrint()
+
+    return vecFromLevel;
+}
+void BST::prettyPrint()
+{
+    int width = getTotalNodes();
+    int height = getHeight();
+
+    std::vector<int> sortedNodes = inOrder();
+
+    for (int i = 0; i < allnodes.size(); i++)
     {
-        int width = getTotalNodes();
-        int height = getHeight();
-
-        std::vector<int> sortedNodes = inOrder();
-
-        for (int i = 0; i < allnodes.size(); i++)
+        for (int j = 0; j < sortedNodes.size(); j++)
         {
-            for (int j = 0; j < sortedNodes.size(); j++)
+            if (sortedNodes.at(j) == allnodes.at(i)->getVal())
             {
-                if (sortedNodes.at(j) == allnodes.at(i)->getVal())
-                {
-                    allnodes.at(i)->setCol(j + 1);
-                }
+                allnodes.at(i)->setCol(j + 1);
             }
         }
-
-        for (int i = 1; i <= height; i++)
-        {
-            printLine(width);
-            printLevel(i);
-        }
-        if (width >= 1)
-        {
-            printLine(width);
-        }
     }
 
-    int main()
+    for (int i = 1; i <= height; i++)
     {
-        BST bst;
-        int num;
-        int srch;
-        std::cout << "Enter the numbers to be stored: ";
+        printLine(width);
+        printLevel(i);
+    }
+    if (width >= 1)
+    {
+        printLine(width);
+    }
+}
+
+int main()
+{
+    BST bst;
+    int num;
+    int srch;
+    std::cout << "Enter the numbers to be stored: ";
+    std::cin >> num;
+
+    if (!std::cin.fail())
+    {
+        bst.insertKey(num);
+    }
+    while (!std::cin.fail())
+    {
         std::cin >> num;
-
         if (!std::cin.fail())
         {
             bst.insertKey(num);
         }
-        while (!std::cin.fail())
-        {
-            std::cin >> num;
-            if (!std::cin.fail())
-            {
-                bst.insertKey(num);
-            }
-        }
-
-        std::cout << "The numbers in sorted order: ";
-        std::vector<int> test = bst.inOrder();
-        if (!test.empty())
-        {
-            for (int i = 0; i < test.size(); i++)
-            {
-                std::cout << test.at(i) << ' ';
-            }
-        }
-        std::cout << std::endl;
-
-        bst.prettyPrint();
-
-        return 0;
     }
+
+    std::cout << "The numbers in sorted order: ";
+    std::vector<int> test = bst.inOrder();
+    if (!test.empty())
+    {
+        for (int i = 0; i < test.size(); i++)
+        {
+            std::cout << test.at(i) << ' ';
+        }
+    }
+    std::cout << std::endl;
+
+    bst.prettyPrint();
+
+    return 0;
+}
